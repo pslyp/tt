1b4374d1bb0c7d4dc9b10c40d625281f36759c78
PSlyp <nonny_038@hotmail.com>
2020-06-15 14:04:07 +0700

    eiei

--- main.py
+++ main.py
@@ -11,4 +11,5 @@ chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
 driver.get("https://www.google.com")
-print(driver.page_source)
+# print(driver.page_source)
+return '{"status": 200}'
