9c7a99b3be58e67112adb8b19f51091bf67eee64
PSlyp <nonny_038@hotmail.com>
2020-06-15 15:49:00 +0700

    sdfs

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import os
 
 # CHROMEDRIVER_PATH = 'D:\Python\chromedriver'
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+if __name__ == '__main__':
+    return ss()
 
-driver.get("https://www.google.com")
-# print(driver.page_source)
-return '{"status": 200}'
+def ss():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+
+    driver.get("https://www.google.com")
+    # print(driver.page_source)
+    return '{"status": 200}'
